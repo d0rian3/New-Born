5ad61dba0ed7826ce911442d57bfc0a2a6969066
d0rian3 <157168434+d0rian3@users.noreply.github.com>
2024-11-18 19:27:59 +0100

    Update price.js

--- scripts/price.js
+++ scripts/price.js
@@ -11,19 +11,19 @@ OrderMini.onclick = () => {
   const service = attrInfo(OrderMini);
   const data = stringify(sample[service]);
   localStorage.setItem("service", data);
-  window.open("/pages/Contact.html", "_self");
+  window.open("../pages/Contact.html", "_self");
 };
 
 OrderStandart.onclick = () => {
   const service = attrInfo(OrderStandart);
   const data = stringify(sample[service]);
   localStorage.setItem("service", data);
-  window.open("/pages/Contact.html", "_self");
+  window.open("../pages/Contact.html", "_self");
 };
 
 OrderPremium.onclick = () => {
   const service = attrInfo(OrderPremium);
   const data = stringify(sample[service]);
   localStorage.setItem("service", data);
-  window.open("/pages/Contact.html", "_self");
+  window.open("../pages/Contact.html", "_self");
 };
